5fc0529ed0981c5ed73269292d9595b3e4eeda63
邱奕玮 <tomhaoye@163.com>
2016-04-23 23:46:10 +0800

    merge sort

--- Merge.php
+++ Merge.php
@@ -1,29 +1,47 @@
 <?php
 
-$arr1 = [21,32,43,54,31,65,76,56,89,91];
+$arr1 = [21,32,54,43,65,31,76,56,89,91];
 $arr2 = [];
 $len = count($arr1);
 
 function merge($arr1,$arr2,$index,$nedex,$ladex){
-	for($k = $index, $j = $nedex + 1; $index < $nedex && $j < $ladex; ++$k){
+	global $arr2;
+	for($k = $index, $j = $nedex; $index < $nedex && $j <= $ladex; ++$k){
 		if($arr1[$index] > $arr1[$j]){
 			$arr2[$k] = $arr1[$j++];
 		}else{
 			$arr2[$k] = $arr1[$index++];
 		}
 	}
-	while($j <= $nedex) $arr2[$k++] = $arr1[$j++];
-	while($j <= $ladex)  $arr2[$k++] = $arr1[$j++];
+	while($j < $nedex) $arr2[$k++] = $arr1[$j++];
+	while($j < $ladex) $arr2[$k++] = $arr1[$j++];
 	print_r($arr2);
 }
 
+// merge($arr1,$arr2,0,1,2);
 
-merge($arr1,$arr2,0,1,9);
-
-function start(){
-	
+function start($arr1,$arr2,$len){
+	global $arr1;
+	$time = 1;
+	while($time < $len){
+		$i = 0;
+		$step = $time;
+		$time = 2*$step;
+		while ($i < $len-$time) {
+			merge($arr1,$arr2,$i,$i+$step,$i+$time);
+			$i = $i+$time;
+		}
+		if($i < $len - $step){  
+            Merge($arr1, $arr2, $i, $i+ $step , $len);
+        }  
+        $tmp = $arr1;
+        $arr1 = $arr2;
+        $arr2 = $tmp;
+		$time++;
+	}
+	print_r($arr1);
 }
 
-start();
+start($arr1,$arr2,$len);
 
 
